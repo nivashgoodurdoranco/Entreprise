e9d59ebfe85cb9b9fabbe0051645fc31fd84d711
nivashgoodurdoranco <nivashgoodur1@gmail.com>
2022-08-26 10:25:59 +0200

    modify delete employe

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -27,7 +27,7 @@ class EmployeController extends AbstractController
             $entityManagerInterface->persist($employer);
             $entityManagerInterface->flush();
 
-            return $this->redirectToRoute('default_home');
+            return $this->redirectToRoute('show_employe');
 
         }
 
@@ -46,5 +46,40 @@ class EmployeController extends AbstractController
         return $this->render('employe/show_employe.html.twig', [
             'employes' => $employes
         ]);
+    }
+
+    #[Route('/modifier-un-employe/{id}/', name: 'edit_employe')]
+    public function editEmploye(EntityManagerInterface $entityManagerInterface, Employe $employe, request $request): Response
+    {
+        $form = $this->createForm(EmployeFormType::class, $employe);
+
+        $form->handleRequest($request);
+
+        // Si le formulaire est envoyé et n'a pas d'erreur
+        if($form->isSubmitted() && $form->isValid()){
+
+            // Sauvegarde des changements faits dans l'article via le manager général des entités
+            $entityManagerInterface->flush();
+
+
+            // Redirection vers la page de l'article modifié
+            return $this->redirectToRoute('show_employe');
+
+        }
+
+        // Appel de la vue en lui envoyant le formulaire à afficher
+        return $this->render('employe/edit_employe.html.twig', [
+            'form_employe' => $form->createView()
+        ]);
+    }
+
+    #[Route('/supprimer-un-employe/{id}/', name: 'delete_employe')]
+    public function deleteEmploye(EntityManagerInterface $em, Employe $employe, Request $request): Response
+    {
+        // Suppression de l'article via le manager général des entités
+        $em->remove($employe);
+        $em->flush();
+
+        return $this->redirectToRoute('show_employe');
     }
 }
